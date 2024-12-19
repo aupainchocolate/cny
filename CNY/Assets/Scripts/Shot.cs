c3183e5eb3cc5aee32550ccdc0402534e2c2c30a
Nadia Yakta <nadia.yakta@learnet.se>
2024-12-19 09:40:09 +0100

    bloub

--- CNY/Assets/Scripts/Shot.cs
+++ CNY/Assets/Scripts/Shot.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class Shot : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = 10f;
     public Rigidbody2D rb;
 
     void Start()
     {
         rb.linearVelocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
