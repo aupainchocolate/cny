2f1421738890185e5b04ee1c4ee12b670dbe0572
Nadia Yakta <nadia.yakta@learnet.se>
2025-02-06 09:47:14 +0100

    edits

--- CNY/Assets/Scripts/Nadia Scripts/PlayerHealth.cs
+++ CNY/Assets/Scripts/Nadia Scripts/PlayerHealth.cs
@@ -13,6 +13,7 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+
         if (health <= 0)
         {
             Destroy(gameObject);
